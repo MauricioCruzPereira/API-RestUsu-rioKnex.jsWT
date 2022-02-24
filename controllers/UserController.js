4b8f9edf237e099620d80226ad140bdccd805570
mauricinhoo <mauriciopereira2310@gmail.com>
2022-02-23 22:16:31 -0300

    Adicionando novos usuários no banco de dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,42 @@
+var User = require("../models/User")
+
 class UserController {
     async index(req, res) {
 
     }
 
     async create(req, res) {
-        console.log(req.body)
+        var { email, name, password } = req.body
+        if (email == undefined) {
+            res.status(400)
+            res.json({ err: "Email é inválido!" })
+            //encerrar o processo
+            return;
+        }
+
+        if (name == undefined) {
+            res.status(400)
+            res.json({ err: "name é inválido!" })
+            return;
+        }
+
+        if (password == undefined) {
+            res.status(400)
+            res.json({ err: "Senha é inválida!" })
+            return;
+        }
+
+        var emailExists = await User.findEmail(email)
+
+        if (emailExists) {
+            res.status(406)
+            res.json({ err: "O email já está cadastrado!" })
+            return
+        }
+
+        await User.new(email, password, name)
+
+        res.status(200)
         res.send("Pegando o corpo da requisição")
     }
 }
